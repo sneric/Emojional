d51f570c62da0a959d72cbe4d7535b2c8811b50f
Eric Smith <sneric.smith@gmail.com>
2019-05-11 11:36:11 -0500

    2nd commit

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        // user clicks on button
+        // click creates a message file
+    
+        let alertController = UIAlertController(title: "title", message: "message", preferredStyle: UIAlertController.Style.alert)
+    
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+    
+    present(alertController, animated: true, completion: nil)
+   
     }
 
 
