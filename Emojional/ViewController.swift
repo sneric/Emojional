26bf5b6ecc0a3de91ab2d0a6d3b6187e7184564f
Eric Smith <sneric.smith@gmail.com>
2019-05-11 10:16:54 -0500

    Initial Commit

---
+++ Emojional/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emojional
+//
+//  Created by ERIC SMITH on 5/11/19.
+//  Copyright © 2019 ERIC SMITH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
