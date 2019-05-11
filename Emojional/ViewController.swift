23f52011e14e6f5b7a237b268afb8cf93c13551a
Eric Smith <sneric.smith@gmail.com>
2019-05-11 13:27:01 -0500

    3rd commit

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -11,6 +11,14 @@ import UIKit
 class ViewController: UIViewController {
     let emojis = ["😱": "I'm afraid", "😃": "I'm happy", "😘": "I'm loved", "🤣": "I'm laughing"]
     
+    let customMessages =
+        ["I'm afraid" : ["It's gonna be ok", "take a deep breath", "How can I help?"],
+         "I'm happy": ["Horay!", "You rock!", "I'm proud of you!"],
+         "I'm loved" : ["Yes you are", "So grateful for you", "Who is the lucky person?"],
+         "I'm laughing" : ["What's so funny?!", "Your laugh is contagious", "haha!"]
+        ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,11 +26,15 @@ class ViewController: UIViewController {
     @IBAction func showMessage(sender: UIButton) {
         // user clicks on button
         // click creates a message file
-        
         let selectedEmotion = sender.titleLabel?.text
+        
+        let emotionDictionary = customMessages[emojis[selectedEmotion!]!]
+        let randomNumber = Int.random(in: 0 ..< emotionDictionary!.count)
+        let emojiMessage = emotionDictionary?[randomNumber]
+        
+        let alertController = UIAlertController(title: "Welcome to your emotion:", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
     
-        let alertController = UIAlertController(title: "Welcome to your emotion:", message: emojis[selectedEmotion!], preferredStyle: UIAlertController.Style.alert)
-    
+        
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
     
         present(alertController, animated: true, completion: nil)
